f58df95d58ee955250ec52d17301167d1b9e1821
Rathtana Duong <drathtana@yahoo.com>
2018-09-26 03:07:35 -0700

    changing res

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(function(req, res, next) {
   next();
 });
 
+app.get('/', (req, res)=> { res.send('it is working') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
