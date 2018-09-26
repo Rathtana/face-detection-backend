de7c54e0344878e4a701d2761d8622f151af5303
Rathtana Duong <drathtana@yahoo.com>
2018-09-26 03:19:33 -0700

    update cors

--- server.js
+++ server.js
@@ -23,12 +23,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT, GET, POST');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.get('/', (req, res)=> { res.send('it is working') })
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
