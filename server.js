e64bfa11fbfc8d31ae7693876baa4a5937b49e8a
Rathtana Duong <drathtana@yahoo.com>
2018-09-26 01:51:08 -0700

    access control

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
